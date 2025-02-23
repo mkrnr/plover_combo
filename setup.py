84a60e587a31cdc0e49063a9fd86e31bc82fc3b8
Martin Körner <info@mkrnr.com>
2025-02-23 07:04:13 +0100

    Add assert for Plover 5.0.0

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+import sys
 
 from plover_build_utils.setup import BuildPy, BuildUi, Develop
+
+try:
+    import plover
+    assert plover.__version__ >= "5.0.0"
+except (ImportError, AssertionError):
+    sys.stderr.write("Error: plover>=5.0.0 is required but not installed.\n")
+    sys.exit(1)
 
 BuildPy.build_dependencies.append('build_ui')
 BuildUi.hooks = ['plover_build_utils.pyqt:fix_icons']
